724d50472de35fcff4944abe726f74e2b893fca1
ramojirao27 <144374184+ramojirao27@users.noreply.github.com>
2024-12-28 10:34:56 +0530

    made changes for vercel

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.post("/user",(req,res)=>{
     res.send("Hello user")
 })
+app.post("login",(req,res)=>{
+    res.send("Thank you for trying to login");
+})
 
 app.listen(5000,()=>{
     console.log("server is running on port 5000");
